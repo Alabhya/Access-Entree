6da1bb855f7af112131544aa55ff0b8a4459536b
Alabhya Maheshwari <Alabhyared@gmail.com>
2021-10-22 13:38:19 -0700

    Removed additional Scripts
    Removed scripts already present in the branch.

--- Assets/Sandbox/Logan/Esteban_scripts/ToolBar_Interactions/Interactable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface Interactable
-{
-    public enum ItemName { None, Axe, Sword};
-    void InteractTools(ItemHandler ItemHandle, ItemName ItemName_);
-}
