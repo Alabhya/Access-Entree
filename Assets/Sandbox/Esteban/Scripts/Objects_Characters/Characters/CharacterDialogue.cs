ddb33dbe701159136f03ec8a614c57968aa5e646
Harlock9651 <88295048+Harlock9651@users.noreply.github.com>
2022-01-07 16:18:36 -0700

    Added Dialogue system to the 'CharacterDialogue' script.
    Integrated the dialogue functionality with Esteban's code.

--- Assets/Sandbox/Esteban/Scripts/Objects_Characters/Characters/CharacterDialogue.cs
+++ Assets/Sandbox/Esteban/Scripts/Objects_Characters/Characters/CharacterDialogue.cs
@@ -7,21 +7,24 @@ public class CharacterDialogue : MonoBehaviour
 {
     public string ObjectToAttachTo = "Character";
     public bool TempActivateMovement;
+	private GameObject player;
+	
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (player == null)
+		{
+			player = GameObject.FindWithTag("Player");
+		}
     }
 
     public void Talk()
     {
         Debug.Log("IS TALKING");
+		if(gameObject.GetComponent("DialogueActivator") != null)
+		{
+			gameObject.GetComponent<DialogueActivator>().Interact(player.GetComponent<DialogueController>());
+		}
     }
 
 }
