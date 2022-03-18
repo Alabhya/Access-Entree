a67b6a7f2fbe905107497aa3020f9da83ecb2e35
Julio Delgado Abreu <JJDelgadoAbreu@student.fullsail.edu>
2022-03-18 14:39:41 -0400

    PlayerInteract.cs updated

--- Assets/Sandbox/Julio/Scripts/PlayerInteract.cs
+++ Assets/Sandbox/Julio/Scripts/PlayerInteract.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // this class will perform a sphere cast from the player position, to the player's forward direction.
 // Once a valid interaction obj is hit we will display an UI button which can be pressed to trigger the custom object interaction logic
@@ -9,11 +10,15 @@ public class PlayerInteract : MonoBehaviour
     private LayerMask interactionLayer;
     private InteractionObj current;
     public float RayCastLenght = 0.75f;
+
+    public Button button;
     // will need whatever UI element represents a button
 
     private void Start() {
         interactionLayer = LayerMask.GetMask("Interaction");
-
+        if (button != null) {
+            button.gameObject.SetActive(false);
+        }
         current = null;
     }
 
@@ -28,6 +33,17 @@ public class PlayerInteract : MonoBehaviour
         RaycastHit hit;
         // this raycast will detect any interactible objects
         Physics.SphereCast(transform.position, RayCastLenght, transform.TransformDirection(Vector3.forward), out hit, 6, interactionLayer);
+        current = hit.transform.GetComponent<InteractionObj>();
+        button.gameObject.SetActive(current != null);
+
+        
+        if (button && button.gameObject.activeInHierarchy) {
+
+        }
+    }
+
+    public void ButtonPress() {
+        Debug.Log("We pressed the button\nInteracting with: " + current.name);
     }
 
 }
