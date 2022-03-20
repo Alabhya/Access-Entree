0ef1d7629cd9b8470b25885d1dd6829aac39d2b8
Muhammad Umar <umarbinnazir@gmail.com>
2022-03-20 15:23:00 +0500

    Implemented Resource Required Panel with static values
    * Right now resource required has some static values
    * Demo Scene contains all changes from Omer
    * Implemented Background Audio in prototype scene

--- Assets/Sandbox/Julio/Scripts/PlayerInteract.cs
+++ Assets/Sandbox/Julio/Scripts/PlayerInteract.cs
@@ -13,13 +13,13 @@ public class PlayerInteract : MonoBehaviour
     [SerializeField]
     private float rayCastDistance = 10.0f;
 
-    public Button button;
+    public GameObject button;
     // will need whatever UI element represents a button
 
     private void Start() {
         interactionLayer = LayerMask.GetMask("Interaction");
         if (button != null) {
-            button.gameObject.SetActive(false);
+            button.SetActive(false);
         }
         current = null;
     }
@@ -33,7 +33,7 @@ public class PlayerInteract : MonoBehaviour
             current = null;
         }
 
-        button.gameObject.SetActive(current != null);
+        button.SetActive(current != null);
         // Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10.0f, Color.yellow);
     }
 
@@ -41,5 +41,4 @@ public class PlayerInteract : MonoBehaviour
        // Debug.Log("We pressed the button");
         current.Interaction();
     }
-
 }
