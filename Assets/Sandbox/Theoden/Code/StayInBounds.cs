2683e341b0963d4fe30616f443e9da44ef502cdf
Harlock9651 <88295048+Harlock9651@users.noreply.github.com>
2022-07-05 21:12:26 -0700

    Stay in Bounds functionality
    Implemented and documented the 'Stay in Bounds' functionality in Area 3.

--- Assets/Sandbox/Theoden/Code/StayInBounds.cs
+++ Assets/Sandbox/Theoden/Code/StayInBounds.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*This script respawns the player at a given spot if they exit the level boundary for whatever reason.
+*/
+
 public class StayInBounds : MonoBehaviour
 {
-	[SerializeField] private Vector3 minVals;
-	[SerializeField] private Vector3 maxVals;
-	[SerializeField] private Vector3 prefVals;
-	[SerializeField] private GameObject player;
-	[SerializeField] private GameObject respawnPoint;
+	[SerializeField] private Vector3 minVals; //The smallest bounds of the level boundary
+	[SerializeField] private Vector3 maxVals; //The larges bounds of the level boundary
+	[SerializeField] private GameObject player; //The player character
+	[SerializeField] private GameObject respawnPoint; //Where the player will respawn should they exit the level boundary
 	
 	[SerializeField] private SceneTransition sTrans; //The LevelLoader object that teleports the player and determines what transition animation is played.
 
@@ -16,16 +18,18 @@ public class StayInBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Check x-values
+        //Check if the player is within the level bounds.
+		//If not, move the player back to the respawn point.
 		if(minVals.x > player.transform.position.x || maxVals.x < player.transform.position.x || minVals.y > player.transform.position.y || maxVals.y < player.transform.position.y || minVals.z > player.transform.position.z || maxVals.z < player.transform.position.z)
 		{
 			Respawn();
 		}
     }
 	
+	//Moves the player to the respawn point
 	private void Respawn()
 	{
-		Debug.Log("Whoopsie doodle");
+		Debug.Log("Whoopsie doodle: Player out of bounds");
 		StartCoroutine(sTrans.ChangeLocation(player, respawnPoint.transform, null));
 	}
 }
