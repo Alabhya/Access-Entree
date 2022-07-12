7c6976b8a72156a79f80e2eac6fc6e8afa75dc79
Harlock9651 <88295048+Harlock9651@users.noreply.github.com>
2022-07-11 22:50:48 -0700

    Update StayInBounds.cs
    Made the requested changes to the StayInBounds code.

--- Assets/Sandbox/Theoden/Code/StayInBounds.cs
+++ Assets/Sandbox/Theoden/Code/StayInBounds.cs
@@ -8,19 +8,21 @@ using UnityEngine;
 public class StayInBounds : MonoBehaviour
 {
 	[SerializeField] private Vector3 minVals; //The smallest bounds of the level boundary
-	[SerializeField] private Vector3 maxVals; //The larges bounds of the level boundary
+	[SerializeField] private Vector3 maxVals; //The largest bounds of the level boundary
 	[SerializeField] private GameObject player; //The player character
 	[SerializeField] private GameObject respawnPoint; //Where the player will respawn should they exit the level boundary
 	
 	[SerializeField] private SceneTransition sTrans; //The LevelLoader object that teleports the player and determines what transition animation is played.
 
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
         //Check if the player is within the level bounds.
 		//If not, move the player back to the respawn point.
-		if(minVals.x > player.transform.position.x || maxVals.x < player.transform.position.x || minVals.y > player.transform.position.y || maxVals.y < player.transform.position.y || minVals.z > player.transform.position.z || maxVals.z < player.transform.position.z)
+		if(	minVals.x > player.transform.position.x || maxVals.x < player.transform.position.x || 
+			minVals.y > player.transform.position.y || maxVals.y < player.transform.position.y || 
+			minVals.z > player.transform.position.z || maxVals.z < player.transform.position.z)
 		{
 			Respawn();
 		}
